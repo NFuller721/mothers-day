8bc1f5a067e587d1bbae67437af6691c25f121c9
NFuller721 <nfuller7218@gmail.com>
2023-05-12 17:24:00 -0400

    Finnish?

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const Body: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center">
+      <p className="mx-16 my-8 text-center text-4xl">
+        You have been a great mom over the past 18 years, and you have always
+        done more for us than we have for you. So thank you for everything you
+        have done.
+      </p>
+      <Image
+        height={498}
+        width={357}
+        src="/ted-lasso-thank-you.gif"
+        alt="'I appreciate that. Thank you.' - Ted Lasso"
+      />
+    </div>
+  );
 };
 
 export default Body;
